f67de9dcef65b3b802ef377667e5522b2aebe5b3
Hussein Tejan <tejanahmedhu@gmail.com>
2018-07-10 14:33:15 +0100

    added functionality to print-account-as-json-branch

--- AccountApplication/src/main/java/com/qa/AccountApplication/App.java
+++ AccountApplication/src/main/java/com/qa/AccountApplication/App.java
@@ -1,13 +1,29 @@
 package com.qa.AccountApplication;
 
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.json.JSONObject;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.qa.accounts.Account;
+import com.qa.service.Service;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello  world to the standard out" );
-    }
+  
+	public static void main(String [] args) {
+		
+		Account a = new Account(0121, "Billy", "Bob", "1234");
+		Service service = new Service();
+		service.create(a);
+		JSONObject json = new JSONObject(service.readAll());
+		System.out.println(json);
+	}
+
+
 }
