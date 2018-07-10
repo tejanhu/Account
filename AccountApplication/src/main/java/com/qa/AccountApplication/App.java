7c48541484fe38f3b890a6ae843e1641544afafc
Hussein Tejan <tejanahmedhu@gmail.com>
2018-07-10 14:46:38 +0100

    added xml conversion

--- AccountApplication/src/main/java/com/qa/AccountApplication/App.java
+++ AccountApplication/src/main/java/com/qa/AccountApplication/App.java
@@ -8,6 +8,8 @@ import org.json.JSONObject;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.qa.accounts.Account;
 import com.qa.service.Service;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
 
 /**
  * Hello world!
@@ -23,6 +25,9 @@ public class App
 		service.create(a);
 		JSONObject json = new JSONObject(service.readAll());
 		System.out.println(json);
+		
+		 XStream xStream = new XStream(new DomDriver());
+         System.out.println(xStream.toXML(a));
 	}
 
 
