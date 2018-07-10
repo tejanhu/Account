7932d68fe4a4922bd1ba3f7e9cd12feb2dee60cd
Hussein Tejan <tejanahmedhu@gmail.com>
2018-07-10 16:35:40 +0100

    Tests all green!

--- AccountApplication/src/main/java/com/qa/service/Service.java
+++ AccountApplication/src/main/java/com/qa/service/Service.java
@@ -35,20 +35,18 @@ public class Service {
 			account_map.remove(id);
 		}
 		
+		public Account findAccount(long id) {
+			
+			Account a = new Account(0000, "", "", "");
+			
+			if(this.account_map.containsKey(id)) {
+				return this.account_map.get(id);
+			}
+			return a;
+		}
+		
 		public int getNameCount(String firstName) {
-//			int counter = 0;
-//			
-//			for(Account account:account_map.values()){
-//				if(account.getFirstName().equals(firstName)) {
-//					counter++;
-//				}
-//			}
-//			
-//			return counter;
-			
 			return (int) this.account_map.values().stream().filter(s -> s.getFirstName().equals(firstName)).count();
-			
-			
 		}
 	
 	
